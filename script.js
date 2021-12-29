b11abbf7d6f98eae2881ee5825b3ea729cf430d9
Lamek Marouf <lamekmaroufjob@hotmail.com>
2021-12-29 12:36:23 +1100

    save button saves user input after refresh

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 window.setInterval(function () {
-    $('#currentDay').html(moment().format('ddd MM/DD/y H:mm:ss'))
+    $('#currentDay').html(moment().format('dddd, Do MMMM y - H:mm:ss'))
 }, 1000);
 
+const storageInput = document.querySelector(".hourText");
+const text = document.querySelector('.text');
+const saveButton = document.querySelector(".saveBtn");
+const storedInput = localStorage.getItem('textInput');
+
+if(storageInput) {
+    text.textContent = storedInput
+}
+
+storageInput.addEventListener("input", letter => {
+    text.textContent = letter.target.value
+});
+
+const saveToLocalStorage = () => {
+    localStorage.setItem('textInput', text.textContent)
+}
+
+saveButton.addEventListener('click', saveToLocalStorage)
+
+
+
+const storageInputTwo = document.querySelector('.hourTextTwo');
+const textTwo = document.querySelector('.textTwo');
+const saveButtonTwo = document.querySelector('.saveBtnTwo');
+const storedInputTwo = localStorage.getItem('textInputTwo');
+
+if(storageInputTwo) {
+    textTwo.textContent = storedInputTwo
+}
+
+storageInputTwo.addEventListener('input', letterTwo => {
+    textTwo.textContent = letterTwo.target.value
+})
+
+const saveToLocalStorageTwo = () => {
+    localStorage.setItem('textInputTwo', textTwo.textContent)
+}
+
+saveButtonTwo.addEventListener('click', saveToLocalStorageTwo)
